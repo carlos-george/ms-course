902fbef450f9393baae8ef3ff9e877b32712256d
CarlosGeorge <carlosgeorge248@gmail.com>
2021-09-11 20:04:35 -0300

    Refactor: Adjusting last commit

---
+++ hr-payroll/src/main/java/br/com/til/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,13 @@
+package br.com.til.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollApplication.class, args);
+	}
+
+}
