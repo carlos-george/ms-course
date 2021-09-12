56ac98034a1709e6a3befd3881cffdd813b00a24
CarlosGeorge <carlosgeorge248@gmail.com>
2021-09-12 10:57:39 -0300

    Feature: Creating project api-gateway-zuul

---
+++ hr-api-gateway-zuul/src/test/java/br/com/til/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.til.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
