1a9672027165f815718055a0dbfaf109d6489cb9
CarlosGeorge <carlosgeorge248@gmail.com>
2021-09-13 18:09:40 -0300

    Feature: Adding Feign Client to hr-oauth

---
+++ hr-oauth/src/main/java/br/com/til/hroauth/services/UserService.java
@@ -0,0 +1,33 @@
+package br.com.til.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.til.hroauth.domain.User;
+import br.com.til.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		
+		User user = userFeignClient.findByEmail(email).getBody();
+		
+		if(user == null) {
+			logger.error("E-mail not found: "+ email);
+			throw new IllegalArgumentException("E-mail not found!");
+		}
+		
+		logger.info("E-mail found: "+ email);
+		
+		return user;
+				
+	}
+}
