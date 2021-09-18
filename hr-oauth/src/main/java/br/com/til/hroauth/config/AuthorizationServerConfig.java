86425722f7b66c71af4398b54535239b008774fa
CarlosGeorge <carlosgeorge248@gmail.com>
2021-09-18 16:19:57 -0300

    Feature: Config security from config-server

--- hr-oauth/src/main/java/br/com/til/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/br/com/til/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,8 @@
  package br.com.til.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +16,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
+@RefreshScope
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Value("${oauth.client.name}")
+	private String clientName;
+	
+	@Value("${oauth.client.secret}")
+	private String clientSecret;
 	
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -39,8 +48,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		
 		clients.inMemory()
-		.withClient("myappname123")
-		.secret(bCryptPasswordEncoder.encode("myappsecret123"))
+		.withClient(clientName)
+		.secret(bCryptPasswordEncoder.encode(clientSecret))
 		.scopes("read", "write")
 		.authorizedGrantTypes("password")
 		.accessTokenValiditySeconds(86400); // 24hrs to expire
